2b7c7baeecd8475214a5c073861d69427b521a6e
Alexejka <tarnsmen90@mail.ru>
2015-01-18 00:26:35 +0200

    #

--- aqua001/src/ua/com/client/Aqua001.java
+++ aqua001/src/ua/com/client/Aqua001.java
@@ -1,5 +1,6 @@
 package ua.com.client;
 
+import ua.com.client.dto.Planet;
 import ua.com.client.dto.Plant;
 import ua.com.client.ui.PlantProgresDialog;
 
@@ -41,6 +42,7 @@ public class Aqua001 implements EntryPoint {
 	 */
 	public void onModuleLoad() {
 		initPlant();
+		initPlanet();
 		DockLayoutPanel mainDockPanel = new DockLayoutPanel(Unit.PX);
 		mainDockPanel.setWidth("100%");
 		mainDockPanel.setHeight("870px");
@@ -52,6 +54,40 @@ public class Aqua001 implements EntryPoint {
 		mainDockPanel.add(getCenterPanel());
 
 		RootPanel.get().add(mainDockPanel);
+	}
+
+	private void initPlanet() {
+		greetingService.getPlanet(new AsyncCallback<Planet>() {
+			
+			@Override
+			public void onSuccess(Planet result) {				
+				Window.alert(toStringCustom(result));
+				
+			}
+			
+			@Override
+			public void onFailure(Throwable caught) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+	}
+	
+	private String toStringCustom(Planet planet) {
+		String resultStr = 
+			planet.getName() + "\n\n" +
+			planet.getParam0().toString() +"\n" + 
+			planet.getParam1().toString() +"\n" + 
+			planet.getParam2().toString() +"\n" + 
+			planet.getParam3().toString() +"\n" + 
+			planet.getParam4().toString() +"\n" + 
+			planet.getParam5().toString() +"\n" + 
+			planet.getParam6().toString() +"\n" + 
+			planet.getParam7().toString() +"\n" + 
+			planet.getParam8().toString() +"\n" + 
+			planet.getParam9().toString();
+		return resultStr;
 	}
 
 	private void initPlant(){
